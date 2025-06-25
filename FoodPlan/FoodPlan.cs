c537cd5b73024125cc5444691410b8eee3a0800e
Maximiliano Peña <maxipenia@gmail.com>
2025-06-25 19:16:58 -0300

    Cambio

--- FoodPlan/FoodPlan.cs
+++ FoodPlan/FoodPlan.cs
@@ -4,7 +4,6 @@ namespace FoodPlan
 {
     public class FoodPlan
     {
-        public static string AzurePassword = "Password-Super-Segura-Para-Leer-Todos-Los-Datos-Productivos!";
         public bool Active { get; set; }
 
         public IList<Meal> Meals { get; set; }
@@ -32,28 +31,6 @@ namespace FoodPlan
         public void AddMeal(Meal meal)
         {
             Meals.Add(meal);
-        }
-
-        public static void Injection()
-        {
-            string userInput = Console.ReadLine();
-
-            string connectionString = "YourConnectionStringHere";
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
-
-                // Vulnerable to SQL Injection due to direct concatenation of user input
-                string query = "SELECT * FROM Users WHERE Username = '" + userInput + "'";
-
-                SqlCommand command = new SqlCommand(query, connection);
-                var reader = command.ExecuteReader();
-
-                while (reader.Read())
-                {
-                    Console.WriteLine("User found: " + reader["Username"]);
-                }
-            }
         }
 
         public void RemoveMealByName(string name)
